f8d46f57ed2a301e26120fae71c4465bdda7b668
Nicolas Hiot <nicolas.hiot@etu.univ-orleans.fr>
2016-11-15 18:54:04 +0100

    Add EvalContext

--- evaluation_context.cpp
+++ evaluation_context.cpp
@@ -1,28 +1,20 @@
-# include "evaluation_context.hpp"
+#include "evaluation_context.hpp"
 
+using namespace std;
 
-using namespace std ;
-
-
-double Evaluation_Context_No_Variable :: get_value ( string const & id ) {
-  assert ( false ) ;
-  return NAN ;
+double Evaluation_Context_No_Variable::get_value(string const &id) {
+  assert(false);
+  return NAN;
 }
 
-
-void Evaluation_Context_No_Variable :: valuate ( string const & id ,
-						 double value ) {}
-
-
-
-double  Evaluation_Context_Simple :: get_value ( string const & id ) { 
-  return NAN ;
+void Evaluation_Context_No_Variable::valuate(string const &id, double value) {
+  assert(false);
 }
 
-
-void  Evaluation_Context_Simple :: valuate ( string const & id ,
-					     double value ) { 
+double Evaluation_Context_Simple::get_value(string const &id) {
+  return valuation.find(id)->second;
 }
 
-
-
+void Evaluation_Context_Simple::valuate(string const &id, double value) {
+  valuation.insert(pair<string, double>(id, value));
+}
