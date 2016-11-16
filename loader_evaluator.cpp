d0a0700c68508b3e9096ef11da5b391b79546307
Nicolas Hiot <nicolas.hiot@etu.univ-orleans.fr>
2016-11-16 14:09:59 +0100

    Add loader

--- loader_evaluator.cpp
+++ loader_evaluator.cpp
@@ -1,67 +1,104 @@
-# include <stack>
-# include <stdlib.h>
+#include <stack>
+#include <stdlib.h>
 
-# include <iostream>
+#include <iostream>
+#include <sstream>
 
-# include "loader_evaluator.hpp"
-# include "exparith_unary.hpp"
-# include "exparith_unary.hpp"
-# include "exparith_binary.hpp"
-# include "exparith_variable.hpp"
+#include "exparith_binary.hpp"
+#include "exparith_unary.hpp"
+#include "exparith_unary.hpp"
+#include "exparith_variable.hpp"
+#include "loader_evaluator.hpp"
 
-# include <assert.h>
+#include <assert.h>
 
-# define NDEBUG 1
+#define NDEBUG 1
 
-using namespace std ; 
+using namespace std;
 
-
-string const sign_read_stop = "." ;
+string const sign_read_stop = ".";
 
 // help write a lot of simular cases
-# define CASE_UNARY( sign , exp )		\
-  else if ( sign == string_read ) {		\
-    assert ( 1 <= l . size () ) ;		\
-    Expr * f = l.top () ; 			\
-    l.pop () ;					\
-    l.push ( new exp ( f ) ) ;			\
+#define CASE_UNARY(sign, exp)                                                  \
+  else if (sign == string_read) {                                              \
+    assert(1 <= l.size());                                                     \
+    Expr *f = l.top();                                                         \
+    l.pop();                                                                   \
+    l.push(new exp(f));                                                        \
   }
 
-
 // help write a lot of simular cases
-# define CASE_BINARY( sign , exp )		  
+#define CASE_BINARY(sign, exp)                                                 \
+  else if (sign == string_read) {                                              \
+    assert(2 <= l.size());                                                     \
+    Expr *right = l.top();                                                     \
+    l.pop();                                                                   \
+    Expr *left = l.top();                                                      \
+    l.pop();                                                                   \
+    l.push(new exp(left, right));                                              \
+  }
 
-    
-Loader_Evaluator :: Loader_Evaluator ( istream & postfixe_stream ) { 
+Loader_Evaluator::Loader_Evaluator(istream &postfixe_stream) {
+  string string_read;
+  stack<Expr *> l;
+  double d;
+
+  while (postfixe_stream >> string_read && string_read != sign_read_stop) {
+    stringstream oss(string_read);
+    if (oss >> d) {
+      l.push(new Constant(d));
+    }
+    CASE_UNARY("log", Log)
+    CASE_UNARY("exp", Exp)
+    CASE_BINARY("+", Add)
+    CASE_BINARY("-", Sub)
+    CASE_BINARY("*", Mul)
+    CASE_BINARY("/", Div)
+    else if (":=" == string_read) {
+      assert(2 <= l.size());
+      Expr *val = l.top();
+      l.pop();
+      Variable *var = (Variable *)l.top();
+      l.pop();
+      l.push(new Set(var, val));
+    }
+    else {
+      l.push(new Variable(string_read));
+    }
+  }
+
+  expression = l.top();
 }
-
-
 
 /*!
- * This is a simple context evaluation (variables are managed correctly) with the special characteristic that if a value is not valuated, it reads the value of the variable on an input stream.
+ * This is a simple context evaluation (variables are managed correctly) with
+ * the special characteristic that if a value is not valuated, it reads the
+ * value of the variable on an input stream.
  */
-class Evaluation_Context_IStream
-  : public Evaluation_Context_Simple {
-  istream & in ;
-public :
-  Evaluation_Context_IStream ( istream & in )
-    : in ( in ) {}
-  /*! Read from the map otherwise from the input stream \c in.
-   */
-  double get_value ( string const & id ) { 
-    return NAN ;
+class Evaluation_Context_IStream : public Evaluation_Context_Simple {
+  istream &in;
+
+public:
+  Evaluation_Context_IStream(istream &in) : in(in) {}
+  /*! Read from the map otherwise from the input stream \c in.*/
+  double get_value(string const &id) {
+    if (valuation.find(id) != valuation.end()) {
+      return valuation[id];
+    } else {
+      double val;
+      if (in >> val) {
+        return val;
+      }
+    }
+    return NAN;
   }
-  
-} ;
+};
 
-
-double Loader_Evaluator :: evaluate ( istream & in ) {
-  Evaluation_Context_IStream ec ( in ) ;
-  return evaluate ( ec ) ;
+double Loader_Evaluator::evaluate(istream &in) {
+  Evaluation_Context_IStream ec(in);
+  return evaluate(ec);
 }
 
-
-double Loader_Evaluator :: evaluate ( Evaluation_Context & ec ) { 
-  return NAN ;
+double Loader_Evaluator::evaluate(Evaluation_Context &ec) {
+  return expression->eval(ec);
 }
-
--- loader_evaluator.hpp
+++ loader_evaluator.hpp
@@ -1,9 +1,10 @@
-# ifndef __LOADER_EVALUATOR_HPP_
-# define __LOADER_EVALUATOR_HPP_
+#ifndef __LOADER_EVALUATOR_HPP_
+#define __LOADER_EVALUATOR_HPP_
 
 /*!
- * \file 
- * This module provides a class to read (from a stream) an expression an store it for evaluation.
+ * \file
+ * This module provides a class to read (from a stream) an expression an store
+ * it for evaluation.
  *
  * \pre For all method, no pointer should NULL
  *
@@ -11,53 +12,49 @@
  * \date 2016
  */
 
+#include <istream>
+#include <list>
 
-# include <list>
-# include <istream>
+#include "exparith.hpp"
 
-# include "exparith.hpp"
-
-# undef NDEBUG
-# include <assert.h>
-
-
+#undef NDEBUG
+#include <assert.h>
 
 /*! Sign making the end of the input of an expression. */
-extern std :: string const sign_read_stop ;
-
+extern std::string const sign_read_stop;
 
 /*!
- * This class is used to read an expression in postfix form on an input stream and then to evaluate it at will.
+ * This class is used to read an expression in postfix form on an input stream
+ * and then to evaluate it at will.
  */
 class Loader_Evaluator {
-  /*! Expression read on the \c postfixe_stream. 
+  /*! Expression read on the \c postfixe_stream.
     Ready for evaluation. */
-  Expr * expression ;
+  Expr *expression;
+
 public:
-  /*! 
+  /*!
    * Constructor.
    * Elements of the expression should be separated by spaces.
    * The expression reading stops when a point (\c '.' ) is read.
    * \param postfixe_stream
    */
-  Loader_Evaluator ( std :: istream & postfixe_stream ) ;
+  Loader_Evaluator(std::istream &postfixe_stream);
   /*!
    * Evaluate the held expression with a given context.
    * \param ec Context for the evaluation.
    * \return The computed value.
    */
-  double evaluate ( Evaluation_Context & ec ) ; 
+  double evaluate(Evaluation_Context &ec);
   /*!
-   * Evaluate the held expression starting from an empty Evaluation_Context_Simple.
+   * Evaluate the held expression starting from an empty
+   * Evaluation_Context_Simple.
    * \param in \c istream to read the value from.
    * \return The computed value.
    */
-  double evaluate ( std :: istream & in ) ;
+  double evaluate(std::istream &in);
   /*! Destructor. */
-  ~ Loader_Evaluator () {
-    delete expression ; 
-  } ; 
-} ; 
+  ~Loader_Evaluator() { delete expression; };
+};
 
-
-# endif
+#endif
